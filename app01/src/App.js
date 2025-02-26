6cd52f960e4b393022def1a2e80480fbcf4fc8e6
rkwhkehd3927 <rkwhkehd3927@naver.com>
2025-02-26 12:15:07 +0900

    250226(react start)

---
+++ app01/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import './css/style.css'; // css 파일 import
+
+function App() {
+  const css01 = {
+    textAlign:'center',
+    
+  }
+
+
+  return (
+    <div>
+      <h2>로그인</h2>
+      <div style={css01} className="d01">aaa</div>
+      <button>확인</button>
+    </div>
+  );
+}
+
+export default App;
